eeded564756860eddee7ea278f888d55e67852dd
Tehchy <juniortec21@yahoo.com>
2019-02-03 02:31:51 -0500

    test

--- editor.js
+++ editor.js
@@ -271,6 +271,7 @@ class ObjectInstance extends THREE.Object3D {
         if (this.boxShape) {
             this.boxShape.position.copy(this.boundingMesh.position);
             //this.boxShape.position.y += this.boundingMesh.scale.y / 2;
+            console.log(this.boundingMesh.scale);
             this.boxShape.scale.copy(this.boundingMesh.scale);
             this.boxShape.rotation.copy(this.boundingMesh.rotation);
         }
